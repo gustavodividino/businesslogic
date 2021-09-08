512079ff89d47f7e9632b1bbb2bfa56001497998
Gustavo Dividino <gustavo.dividino@telefonica.com>
2021-09-08 11:09:17 -0300

    .

--- frontend-nextjs/src/components/Header/index.tsx
+++ frontend-nextjs/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from "next/link";
 
 import styles from './styles.module.scss';
 
@@ -11,6 +12,17 @@ export function Header() {
                     <img src="/mind-map.png"></img>
                     <h3>MindMap - IntermediatE</h3>
                 </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/"><i className="las la-home"></i><small>Início</small></a>
+                        </li>
+                        <Link href="#">
+                            <a target="_blank" href="https://telefonicacorp-my.sharepoint.com/:f:/g/personal/andrius_ribas_telefonica_com/Ep84usjJGPFOmGSITNJY290B06K-vXgp6_o6h3A6uRnPSw?e=OV8exW"><i className="las la-folder"></i><small>Documentações</small> </a>
+                        </Link>
+
+                    </ul>
+                </nav>
             </section>
         </header>
     )
